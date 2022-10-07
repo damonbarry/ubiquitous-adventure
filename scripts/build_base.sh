6808802ae739cc53b03726c1051227349cb5f2c9
Damon Barry <damonb@microsoft.com>
2022-10-07 12:30:27 -0700

    Fix script to push image

--- scripts/build_base.sh
+++ scripts/build_base.sh
@@ -6,4 +6,4 @@ context_path=$(realpath "$(dirname "$0")/../images/base")
 docker buildx create --use --bootstrap
 trap "docker buildx rm" EXIT
 
-docker buildx build --no-cache --output='type=docker,name=damonbarry/base,buildinfo-attrs=true' "$context_path"
+docker buildx build --no-cache --output='type=image,push=true,name=damonbarry/base,buildinfo-attrs=true' "$context_path"
